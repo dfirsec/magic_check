6f7225b1962434780805c36d2329d744cf4008e5
Mitch <12067255+dfirsec@users.noreply.github.com>
2020-08-11 10:51:06 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,94 @@
+## Installation
+
+```text
+git clone https://github.com/dfirsec/magic_check.git
+cd magic_check
+pip install -r requirements.txt
+```
+
+### Simple Usage
+```text
+python check_magic.py -p {FILE PATH}
+```
+#### Option
+```text
+python check_magic.py -p {FILE PATH} -f {FILE FORMAT}
+```
+
+### Check for all file formats (from list below)
+
+```text
+python check_magic.py d:\Downloads\Ultimate_Guide_Arduino_Sensors_Modules      
+
+ PDF   d:\Downloads\Ultimate_Guide_Arduino\Ultimate_Guide_Arduino_Sensors_Modules.pdf
+ ZIP   d:\Downloads\Ultimate_Guide_Arduino\1 - DHT11_DHT22\Code\DHT_library.zip
+ JFIF  d:\Downloads\Ultimate_Guide_Arduino\1 - DHT11_DHT22\Schematics\Schematics.jpg
+ PNG   d:\Downloads\Ultimate_Guide_Arduino\10 - Tilt\Schematics\Schematics.png
+ PNG   d:\Downloads\Ultimate_Guide_Arduino\11 - Microphone Sound\Schematics\Schematics.png
+ PNG   d:\Downloads\Ultimate_Guide_Arduino\12 - Reed Switch\Schematics\Magnetic_Reed_Switch.png
+ ZIP   d:\Downloads\Ultimate_Guide_Arduino\13 - MRFC522 RFID\Code\RFID_Library.zip
+ PNG   d:\Downloads\Ultimate_Guide_Arduino\13 - MRFC522 RFID\Schematics\RFID.png
+ PNG   d:\Downloads\Ultimate_Guide_Arduino\14 - Relay\Schematics\Relay_Module.png
+ ...
+ ```
+ 
+ ### Check by specified file format (from list below)...in this example, a ```zip``` file.
+ 
+ ```text
+python check_magic.py d:\Downloads\Ultimate_Guide_Arduino_Sensors_Modules -f zip
+
+d:\Downloads\Ultimate_Guide_Arduino_Sensors_Modules\1 - DHT11_DHT22\Code\DHT_library.zip
+d:\Downloads\Ultimate_Guide_Arduino_Sensors_Modules\13 - MRFC522 RFID\Code\RFID_Library.zip
+d:\Downloads\Ultimate_Guide_Arduino_Sensors_Modules\15 - nRF24L01\Code\RadioHead_Library.zip
+d:\Downloads\Ultimate_Guide_Arduino_Sensors_Modules\16 - 433 MHZ Transmitter_Receiver\Code\RadioHead_Library.zip
+d:\Downloads\Ultimate_Guide_Arduino_Sensors_Modules\18 - Dot Matrix\Code\LedControl.zip
+...
+```
+
+### File Formats and Magic Signature:
+
+```text
+file_types = {
+    '7z': b'37 7a bc af 27 1c',
+    'aac': b'41 41 43 00 01 00',
+    'asm': b'00 61 73 6d',
+    'avi': b'52 49 46 46',
+    'au': b'64 6e 73 2e',
+    'bin': b'53 50 30 31',
+    'exe': b'4d 5a',
+    'bmp': b'42 4d',
+    'bz2': b'42 5a 68',
+    'dat': b'50 4d 4f 43 43 4d 4f 43',
+    'deb': b'21 3c 61 72 63 68 3e',
+    'doc': b'cf 11 e0 a1 b1 1a e1 00',
+    'elf': b'7f 45 4c 46',
+    'flash': b'43 57 53',
+    'flv': b'46 4c 56 01',
+    'gif': b'47 49 46 38 37 61',
+    'jfif': b'ff d8 ff e0 00 10 4a 46 49 46 00 01',
+    'jpg': b'ff d8 ff db',
+    'mkv': b'1a 45 df a3',
+    'mp3': b'49 44 33',
+    'mp3_hd': b'49 44 33 03 00 00 00',
+    'mpeg': b'00 00 01 ba',
+    'mpg2': b'00 00 01 ba 44',
+    'ogg': b'4f 67 67 53',
+    'pcap': b'd4 c3 b2 a1',
+    'pcapng': b'0a 0d 0d 0a',
+    'pdf': b'25 50 44 46 2d',
+    'png': b'89 50 4e 47 0d 0a 1a 0a',
+    'rar': b'52 61 72 21 1a 07 01 00',
+    'raw': b'52 41 57 41 44 41 54 41',
+    'reg': b'72 65 67 66',
+    'rtf': b'7b 5c 72 74 66 31',
+    'sqlite': b'53 51 4c 69 74 65 20 66 6f 72 6d 61 74 20 33 00',
+    'tarv1': b'75 73 74 61 72 00 30 30',
+    'tarv2': b'75 73 74 61 72 20 20 00',
+    'vmdk': b'4b 44 4d',
+    'vmem': b'53 ff 00 f0',
+    'wmv': b'30 26 b2 75 8e 66 cf 11 a6 d9 00 aa 00 62 ce 6c',
+    'xar': b'78 61 72 21',
+    'xml': b'3c 3f 78 6d 6c 20',
+    'zip': b'50 4b 03 04'
+}
+```
---
+++ magic_check.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+
+__author__ = 'DFIRSec (@pulsecode)'
+__description__ = 'Confirm file type by matching the magic signature ("number")'
+__version__ = "1.2"
+
+
+import argparse
+import binascii
+import os
+import sys
+
+from colorama import Fore, Style, init
+
+# Ref: https://www.garykessler.net/library/file_sigs.html
+
+file_types = {
+    '7z': b'37 7a bc af 27 1c',
+    'aac': b'41 41 43 00 01 00',
+    'asm': b'00 61 73 6d',
+    'avi': b'52 49 46 46',
+    'au': b'64 6e 73 2e',
+    'bin': b'53 50 30 31',
+    'exe': b'4d 5a',
+    'bmp': b'42 4d',
+    'bz2': b'42 5a 68',
+    'dat': b'50 4d 4f 43 43 4d 4f 43',
+    'deb': b'21 3c 61 72 63 68 3e',
+    'doc': b'cf 11 e0 a1 b1 1a e1 00',
+    'elf': b'7f 45 4c 46',
+    'flash': b'43 57 53',
+    'flv': b'46 4c 56 01',
+    'gif': b'47 49 46 38',
+    'jfif': b'ff d8 ff e0 00 10 4a 46 49 46 00 01',
+    'jpg': b'ff d8 ff db',
+    'mkv': b'1a 45 df a3',
+    'mp3': b'49 44 33',
+    'mp3_hd': b'49 44 33 03 00 00 00',
+    'mpeg': b'00 00 01 ba',
+    'mpg2': b'00 00 01 ba 44',
+    'ogg': b'4f 67 67 53',
+    'ovpn': b'63 6c 69 65 6e 74 0a 64 65 76',
+    'pcap': b'd4 c3 b2 a1',
+    'pcapng': b'0a 0d 0d 0a',
+    'pdf': b'25 50 44 46 2d',
+    'png': b'89 50 4e 47 0d 0a 1a 0a',
+    'rar': b'52 61 72 21 1a 07 01 00',
+    'raw': b'52 41 57 41 44 41 54 41',
+    'reg': b'72 65 67 66',
+    'rtf': b'7b 5c 72 74 66 31',
+    'sqlite': b'53 51 4c 69 74 65',
+    'tar': b'75 73 74 61 72',
+    'tor': b'64 38 3a 61 6e 6e 6f 75 6e 63 65',
+    'vmdk': b'4b 44 4d',
+    'vmem': b'53 ff 00 f0',
+    'webp': b'52 49 46 46 94 45 0a 00 57 45 42 50 56 50 38',
+    'wmv': b'30 26 b2 75 8e 66 cf 11 a6 d9 00 aa 00 62 ce 6c',
+    'xar': b'78 61 72 21',
+    'xml': b'3c 3f 78 6d 6c',
+    'zip': b'50 4b 03 04'
+}
+
+# Initizlize colorama
+init()
+
+
+class Termcolor:
+    # Unicode Symbols and colors
+    BOLD = Fore.LIGHTWHITE_EX
+    CYAN = Fore.CYAN
+    GRAY = Fore.LIGHTBLACK_EX
+    GREEN = Fore.LIGHTGREEN_EX
+    RED = Fore.RED
+    YELLOW = Fore.LIGHTYELLOW_EX
+    RESET = Style.RESET_ALL
+
+
+# Initialize termcolors
+tc = Termcolor()
+
+
+def scan_dir(path):
+    try:
+        for files in os.scandir(path):
+            try:
+                with open(files, 'rb') as fd:
+                    file_head = fd.read(20)
+                    yield binascii.hexlify(file_head, ' '), files.path
+            except KeyboardInterrupt:
+                sys.exit("\nExited")
+            except Exception:
+                continue
+    except PermissionError as e:
+        print(e)
+
+
+def main(path, filetype=None):
+    found = []
+    for x in scan_dir(path):
+        try:
+            if filetype:
+                if file_types[filetype] in x[0]:
+                    found.append(x[1])
+            else:
+                for k, v in file_types.items():
+                    if v in x[0]:
+                        print(f" {tc.BOLD}{k.upper():7}{tc.RESET}{x[1]}")
+        except KeyError:
+            sys.exit(f"{tc.RED}[ERROR]{tc.RESET} File format '{tc.YELLOW}{filetype}{tc.RESET}' is not an available selection.")  # nopep8
+        except KeyboardInterrupt:
+            sys.exit("\nExited")
+
+    if found and found != None:
+        print('\n'.join([x for x in found]))
+    else:
+        print(f"Nothing found for file type: '{filetype}'")
+
+
+if __name__ == '__main__':
+    banner = fr"""
+        __  ___            _         ________              __  
+       /  |/  /___ _____ _(_)____   / ____/ /_  ___  _____/ /__
+      / /|_/ / __ `/ __ `/ / ___/  / /   / __ \/ _ \/ ___/ //_/
+     / /  / / /_/ / /_/ / / /__   / /___/ / / /  __/ /__/ ,<
+    /_/  /_/\__,_/\__, /_/\___/   \____/_/ /_/\___/\___/_/|_|
+                 /____/
+                                                v{__version__}
+                                                {__author__}
+    """
+
+    print(f"{tc.CYAN}{banner}{tc.RESET}")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', dest='path', help="Path to search")
+    parser.add_argument('-f', '--filetype', help="file type selector")
+    parser.add_argument('-l', '--listtype',
+                        action='store_true', help="list file types")
+
+    args = parser.parse_args()
+
+    if len(sys.argv[1:]) == 0:
+        parser.print_help()
+        parser.exit()
+
+    if args.listtype:
+        print(f"{tc.GREEN}Choose from the following:{tc.RESET}")
+        for k in file_types:
+            print(f" {tc.GRAY}-{tc.RESET} {tc.BOLD}{k}{tc.RESET}")
+    elif args.filetype:
+        main(args.path, args.filetype.lower())
+    else:
+        main(args.path)
